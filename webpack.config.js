946c61e788bf083f9eaa515bf381506440cecf47
Julien Plouvier <julien3plouvier@gmail.com>
2018-05-28 16:16:11 +0200

    site opens on run + setup dev and prod env

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+const config = require('./config')
+const path = require('path')
+
+const productionMode = process.env.WEBPACK_MODE == "production"
+
+function assetsPath(_path) {
+    var assetsSubDirectory = productionMode
+        ? config.build.assetsSubDirectory
+        : config.dev.assetsSubDirectory
+    return path.posix.join(assetsSubDirectory, _path)
+}
+
 module.exports = {
     entry: "./src/index.tsx",
+    mode: process.env.WEBPACK_MODE,
+    context: __dirname,
+    devtool: config.build.productionSourceMap ? 'source-map' : false,
     output: {
         filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: config.build.assetsRoot,
+        publicPath: "/",
+        filename: assetsPath('js/[name].[hash].js'),
+        chunkFilename: assetsPath('js/[name].[chunkhash].js')
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -22,7 +40,12 @@ module.exports = {
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
-
+    devServer: {
+        historyApiFallback: true,
+        // contentBase: config.build.assetsRoot,
+        open: true,
+        hot: true
+    },
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
